cd057e041ee475aab51aa3b2f5d77e25daeb674d
Madhu <Madhu@192.168.0.108>
2018-12-07 10:33:51 +0530

    added User Model

--- SpringMVCOne/src/controllers/SignupController.java
+++ SpringMVCOne/src/controllers/SignupController.java
@@ -1,10 +1,13 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import models.User;
 
 @Controller
 public class SignupController {
@@ -13,14 +16,28 @@ public class SignupController {
 		return "Signup";
 	}
 
-	@RequestMapping(value = "/trysignup", method = RequestMethod.POST)
-	public ModelAndView trySignUp(@RequestParam String name,
-			                      @RequestParam String email, 
-			                      @RequestParam Long phone) {
-		ModelAndView m1 = new ModelAndView("UserProfileView");
-		m1.addObject("name", name);
-		m1.addObject("email", email);
-		m1.addObject("phone", phone);
-		return m1;
-	}
+	/*
+	 * @RequestMapping(value = "/trysignup", method = RequestMethod.POST) public
+	 * void trySignUp(@RequestParam String name,
+	 * 
+	 * @RequestParam String email,
+	 * 
+	 * @RequestParam Long phone) { System.out.println(name);
+	 * System.out.println(email); System.out.println(phone); }
+	 */
+
+	
+	 @RequestMapping(value = "/trysignup", method = RequestMethod.POST) public
+	 void trySignUp(@ModelAttribute User u1) 
+	 { System.out.println(u1.getName());
+	    System.out.println(u1.getEmail()); 
+	   System.out.println(u1.getPhn());
+	 }
+	 
 }
+
+
+
+
+
+
---
+++ SpringMVCOne/src/models/User.java
@@ -0,0 +1,32 @@
+package models;
+
+public class User {
+
+	String name;
+	String email;
+	Long phn;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getPhn() {
+		return phn;
+	}
+
+	public void setPhn(Long phn) {
+		this.phn = phn;
+	}
+}
